946062499c2e0d895bcf74edc730dc483acb064b
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-18 12:22:46 +0200

    add new tables to schema.sql and updated animals table

--- schema.sql
+++ schema.sql
@@ -10,5 +10,28 @@ CREATE TABLE animals(
   PRIMARY KEY(id)
 );
 
+-----
+
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(250);
+
+-----
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+ ADD COLUMN species_id  INT references species(id);
+
+ ALTER TABLE animals 
+ ADD COLUMN owner_id  INT references owners(id);
+------------------------------------------------
+CREATE TABLE owners (
+    id			SERIAL PRIMARY KEY,
+	full_name	VARCHAR(250),
+	age 		INT
+)
+------------------------------------------------
+CREATE TABLE species (
+    id			SERIAL PRIMARY KEY,
+	name	    VARCHAR(250)
+)
